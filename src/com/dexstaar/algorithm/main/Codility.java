28e6ed7467a2486556546a8c65bf2160d63c2480
dexstar <dexstar@192.168.0.149>
2016-05-01 20:45:53 +0900

    solved BinaryGap

---
+++ src/com/dexstaar/algorithm/main/Codility.java
@@ -0,0 +1,12 @@
+package com.dexstaar.algorithm.main;
+import com.dexstaar.codility.*;
+
+public class Codility {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BinaryGap binaryGap = new BinaryGap();
+		System.out.println( binaryGap.solution(6));
+	}
+
+}
