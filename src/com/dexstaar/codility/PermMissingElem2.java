c879f5b23eff12befa548839c0e9f1a3954d6280
dexstar <dexstar@192.168.0.149>
2016-05-08 17:00:24 +0900

    a

--- src/com/dexstaar/codility/PermMissingElem2.java
+++ src/com/dexstaar/codility/PermMissingElem2.java
@@ -14,14 +14,15 @@ public class PermMissingElem2 {
 		Arrays.sort(A);
 		
 		// 주어진 배열의 합
-		int sum = 0;
+		long sum = 0;
 		for(int elem : A) sum += elem;
 		
 		// 원래의 합 
-		int lastElem = A[A.length-1];
-		int idealSum = ((lastElem+1)*lastElem) / 2;
+		long lastElem = A[A.length-1];
+		long idealSum = ((lastElem+1)*lastElem) / 2;
 		
-		return idealSum - sum;
+		long returnNum = idealSum - sum;
+		return (int) returnNum;
 		
 		
     }
