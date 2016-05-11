8a60f7df5ce9206c22e016c7236315b174127c2d
dexter <dexter@dexstar-e320>
2016-05-11 13:07:25 +0900

    a

--- src/com/dexstaar/codility/MissingInteger.java
+++ src/com/dexstaar/codility/MissingInteger.java
@@ -1,5 +1,6 @@
 package com.dexstaar.codility;
 
+import java.util.*;
 
 public class MissingInteger {
 	public MissingInteger(){
@@ -8,19 +9,29 @@ public class MissingInteger {
 	
 	public int solution(int[] A) {
         // write your code in Java SE 8
-		int result = 1;
-		
-		int[] counter = new int[A.length+1];
-		
-		for(int i=0; i<A.length; i++){
-			counter[A[i]] = counter[A[i]] + 1;
-			//System.out.println("counter["+A[i]+"] = "+counter[A[i]]);
-		}
-		
-		for(int i=1; i<counter.length; i++){
-			if( counter[i] == 0 ) result = i;
-		}
-		
-		return result;
+		Set<Integer> set = new HashSet<Integer>();
+
+        for(int x:A) {
+            set.add(x);
+        } 
+
+        int maximum = Collections.max(set);
+
+        while (maximum <= 0) {
+            maximum ++;
+        }
+
+        int num = maximum + 1;
+
+        for(int i=1; i<=maximum; i++) {
+            num = i;
+            if(set.contains(num)) {
+                num++;
+            }
+            else {
+                break;
+            }
+        }
+        return num;
     }
 }
