8380e61a2a59be6497f3414a0c0df4d04e8d46a1
dexter <dexter@dexstar-e320>
2016-06-07 20:43:10 +0900

    a

---
+++ src/com/dexstaar/algorithm/main/Executor.java
@@ -0,0 +1,14 @@
+package com.dexstaar.algorithm.main;
+
+import com.dexstaar.algorithm.resursion.*;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Permutation permutation = new Permutation("abc");
+		permutation.permute();
+
+	}
+
+}
